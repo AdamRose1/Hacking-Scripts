033b94929b16af2206ed7802f80a2cb13d182264
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-02-13 16:57:19 -0500

    Update Get_All_Subdomains_In_Scope.sh

--- Get_All_Subdomains_In_Scope.sh
+++ Get_All_Subdomains_In_Scope.sh
@@ -47,9 +47,9 @@ cat step1 | while read -r line;do if ! grep -qix "$line" formatted_out_of_scope.
 # If dns lookup does not resolve then remove from the subdomain from the in scope list found in the file step2
 for subd in $(cat step2);do nslookup_output=$(nslookup "$subd");if [[ ! "$nslookup_output" =~ "server can't find" ]];then echo $subd >> step3;fi;done
 
-# Remove any subdomain in the list that have a CNAME pointing out of scope (Only elminiated this for the shodan output, now need to eliminate the rest of the output gathered)
-for ip in $(cat step3);do dig "$ip"|grep -i cname|awk '{print $5}'| sed 's/\.$//g'|grep -i $target;done
+# Remove any subdomain in the list that has a CNAME pointing out of scope
+for ip in $(cat step3);do if dig "$ip"|grep -qi cname|awk '{print $5}'| sed 's/\.$//g' | grep -qi $target;then echo $ip >>step4; elif ! dig "$ip" | grep -qi cname > /dev/null; then echo $ip >> step4; fi;done
 
 # Convert from domain name to IP (geoiplookup does not work with domain name) and then do geoip lookup.
-echo 'Whatever is output to this file means that it is not in the US and is therefore out of scope.' > step5 
-for ip in $(cat step3);do host $ip | grep "address"|awk '{print $NF}' >> step4;done && cat step4 | xargs -I {} geoiplookup {}|grep -v "US\|can't resolve" >> step5 
+echo 'Whatever is output to this file means that it is not in the US and is therefore out of scope.' > step6 
+for ip in $(cat step4);do host $ip | grep "address"|awk '{print $NF}' >> step5;done && cat step5 | xargs -I {} geoiplookup {}|grep -v "US\|can't resolve" >> step6 
