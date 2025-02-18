255eb97b465c4f9c788285b003ffbb4bda2c7448
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-02-18 16:11:50 -0500

    Update Scan_Network.sh

--- Scan_Network.sh
+++ Scan_Network.sh
@@ -6,7 +6,8 @@ target="-iL /must/be/full/path/step7-final_results"
 speed="" # If left blank then nmap will use the default speed
 protocol=""  # Leave empty if you want to use TCP. For UDP enter -sU here.
 host_discovery="-sn" # Change to empty if using UDP protocol
-httpx_scan="yes"
+httpx_scan="yes" 
+httpx_scan_with_screenshot="yes" # Use httpx with -ss to get screenshot of websites httpx found
 ports_to_scan="" # If left blank nmap will use the default. Can put -p- for all 65k ports
 
 
@@ -23,14 +24,27 @@ for ip in $(cat ../nmap_host-discovery|grep 'scan report' | awk '{print $5}');do
 mkdir step3_sCV && cd step3_sCV
 for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do sudo nmap -Pn $ip $speed $protocol -p $ports -sCV -oN nmap_sCV_$ip;done;done
 
-# httpx scan to find ports that are serving websites 
+# httpx scan to find ports that are serving websites. 
 if [[ "$httpx_scan" == "yes" || "$httpx_scan" == "y" ]]; then
-    for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx -u $ip -p $ports -title -status-code -tech-detect -follow-redirects -ip -method -o httpx_output_$ip;done;done
+    if [[ "httpx_scan_with_screenshot" == "yes" || "httpx_scan_with_screenshot" =="y" ]];then
+        for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do echo $ip >> httpx_targets.txt;done
+        for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx -l httpx_targets.txt -p $ports -ss -title -status-code -tech-detect                
+        -follow-redirects -ip -method;done
+        mv output/screenshot/screenshot.html ../../../../httpx_output_all_targets_screenshots.html
+        # Create a directory for each host discovered with open ports, and then move each nmap file output to the target directory.  This is helpful for organizing notes on large subnets
+        mkdir all_targets && cd all_targets
+        for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir -p $ip/enumeration && cp ../nmap_sCV_$ip $ip/enumeration/;done
+    else
+        for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx  
+        -u $ip -p $ports -title -status-code -tech-detect -follow-redirects -ip -method -o httpx_output_$ip;done;done 
+        # Create a directory for each host discovered with open ports, and then move each nmap and httpx file ouptut to the target directory. This is helpful for organizing notes on large subnets
+        mkdir all_targets && cd all_targets
+        for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir -p $ip/enumeration && cp ../nmap_sCV_$ip ../httpx_output_$ip $ip/enumeration/;done
+else
+    # Create a directory for each host discovered with open ports, and then move each nmap file output to the target directory.  This is helpful for organizing notes on large subnets
+    mkdir all_targets && cd all_targets
+    for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir -p $ip/enumeration && cp ../nmap_sCV_$ip $ip/enumeration/;done
 fi
-
-# Create a directory for each host discovered with open ports, and then move each nmap file output to the target directory.  This is helpful for organizing notes on large subnets
-mkdir all_targets && cd all_targets
-for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir -p $ip/enumeration && cp ../nmap_sCV_$ip ../httpx_output_$ip $ip/enumeration/;done
 
 # Organize, not deleting the step1-3 directories so that issues can be debugged if needed
 cd ../ && mv all_targets ../../../ && cd ../../../
