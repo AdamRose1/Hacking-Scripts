e77a58b414df5ab68308037ab4235e8ad899f42e
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-02-20 06:20:43 -0500

    Update Scan_Network.sh

--- Scan_Network.sh
+++ Scan_Network.sh
@@ -6,8 +6,7 @@ target="-iL /must/be/full/path/step7-final_results"
 speed="" # If left blank then nmap will use the default speed
 protocol=""  # Leave empty if you want to use TCP. For UDP enter -sU here.
 host_discovery="-sn" # Change to empty if using UDP protocol
-httpx_scan="yes" 
-httpx_scan_with_screenshot="yes" # Use httpx with -ss to get screenshot of websites httpx found
+httpx_scan="yes" # Enter 'yes' to run httpx scan. 
 ports_to_scan="" # If left blank nmap will use the default. Can put -p- for all 65k ports
 
 
@@ -25,14 +24,11 @@ mkdir step3_sCV && cd step3_sCV
 for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do sudo nmap -Pn $ip $speed $protocol -p $ports -sCV -oN nmap_sCV_$ip;done;done
 
 # httpx scan to find ports that are serving websites. 
-if [[ "$httpx_scan" == "yes" || "$httpx_scan" == "y" ]]; then
-    if [[ "httpx_scan_with_screenshot" == "yes" || "httpx_scan_with_screenshot" =="y" ]];then
-        for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do echo $ip >> httpx_targets.txt;done
-        for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx -l httpx_targets.txt -p $ports -ss -title -status-code -tech-detect -follow-redirects -ip -method;done && mv output/screenshot/ ../../../httpx_output_all_targets_screenshots
-    else :
-    
-    for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx  
-    -u $ip -p $ports -title -status-code -tech-detect -follow-redirects -ip -method -o httpx_output_$ip;done;done 
+httpx_scan=$(echo "$httpx_scan"| tr '[:upper:]' '[:lower:]')
+
+if [[ "$httpx_scan" == "yes" || "$httpx_scan" == "y" ]]; then  
+    for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do httpx -u $ip -p $ports -title -status-code -tech-detect -follow-redirects -ip -method -o httpx_output_$ip -ss;done;done 
+    mv output/screenshot/ ../../../httpx_output_all_targets_screenshots
     
     # Create a directory for each host discovered with open ports, and then move each nmap and httpx file ouptut to the target directory. This is helpful for organizing notes on large subnets
     mkdir all_targets && cd all_targets
@@ -43,8 +39,6 @@ else
     for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir -p $ip/enumeration && cp ../nmap_sCV_$ip $ip/enumeration/;done
 fi
 
-# Organize, not deleting the step1-3 directories so that issues can be debugged if needed
+# Organize final results
 cd ../ && mv all_targets ../../../ && cd ../../../
-
-# If you do not want to create the below empty files in each target IP, then comment out the line below
 for ip in $(ls all_targets);do touch all_targets/$ip/enumeration/enumeration.txt all_targets/$ip/exploit_path.txt all_targets/$ip/creds.txt;done
